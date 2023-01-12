23caa69f44f720ab73bb31eb3e3138dc7bd55c07
mustafa <mryuksel5@gmail.com>
2023-01-13 00:12:19 +0300

    Code refactoring - component klasörü oluşturuldu

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,6 +3,8 @@ import 'package:firebase_chat_app/screens/login_screen.dart';
 import 'package:firebase_chat_app/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../components/rounded_button.dart';
+
 class WelcomeScreen extends StatefulWidget {
   static String id = "welcome_screen";
 
@@ -84,26 +86,4 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   }
 }
 
-class RoundedButton extends StatelessWidget {
-  Color color;
-  Text text;
-  VoidCallback function;
-  RoundedButton(this.color, this.function, this.text);
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        elevation: 5.0,
-        color: color,
-        borderRadius: BorderRadius.circular(30.0),
-        child: MaterialButton(
-          onPressed: function,
-          minWidth: 200.0,
-          height: 42.0,
-          child: text
-        ),
-      ),
-    );
-  }
-}
+
