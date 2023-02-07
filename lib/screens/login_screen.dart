44b2986f7ccdbbd7a138ef761f69ed1725d40609
mustafa <mryuksel5@gmail.com>
2023-02-08 01:11:32 +0300

    firebase paketleri eklendi

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,58 +11,63 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  
   @override
   Widget build(BuildContext context) {
+   
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: "logo",
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: Hero(
+                  tag: "logo",
+                  child: Image(
+                    image: AssetImage('images/logo.png')
+                  )
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kInputDecoration.copyWith(
-                hintText: "Enter your e mail",
+              const SizedBox(
+                height: 28.0,
               ),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kInputDecoration.copyWith(
-                hintText: "Enter your password"
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kInputDecoration.copyWith(
+                  hintText: "Enter your e mail",
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              Colors.lightBlueAccent,
-              () {
-                //Implement login functionality.
-              },
-              const Text(
-                'Log In',
+              const SizedBox(
+                height: 8.0,
               ),
-            )
-          ],
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kInputDecoration.copyWith(
+                  hintText: "Enter your password"
+                ),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                Colors.lightBlueAccent,
+                () {
+                  //Implement login functionality.
+                },
+                const Text(
+                  'Log In',
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
