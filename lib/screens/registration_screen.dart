58a87d00d8946bf534265457a26f560a2b1e44cc
mustafa <mryuksel5@gmail.com>
2023-02-08 20:05:54 +0300

    register page uı

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -15,69 +15,73 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            const Hero(
-              tag: "logo",
-              child: SizedBox(
-                height: 200.0,
-                child: Image(
-                  image: AssetImage('images/logo.png'),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: const Hero(
+                  tag: "logo",
+                  child: SizedBox(
+                    height: 200.0,
+                    child: Image(
+                      image: AssetImage('images/logo.png'),
+                    ),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              style: const TextStyle(
-                color: Colors.black
+              const SizedBox(
+                height: 48.0,
               ),
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kInputDecoration.copyWith(
-                hintText: "Enter your e-mail"
-              )
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.black
+              TextField(
+                style: const TextStyle(
+                  color: Colors.black
+                ),
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kInputDecoration.copyWith(
+                  hintText: "Enter your e-mail"
+                )
               ),
-              obscureText: true,
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kInputDecoration.copyWith(
-                hintText: "Enter your password",
-              )
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-
-            RoundedButton(
-              Colors.blueAccent,
-              () {
-                //Implement registration functionality.
-                
-              },
-
-              const Text("Register"),
-           
-            ),
-          ],
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.black
+                ),
+                obscureText: true,
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kInputDecoration.copyWith(
+                  hintText: "Enter your password",
+                )
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+    
+              RoundedButton(
+                Colors.blueAccent,
+                () {
+                  //Implement registration functionality.
+                  
+                },
+    
+                const Text("Register"),
+             
+              ),
+            ],
+          ),
         ),
       ),
     );
