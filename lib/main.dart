937f487d7820062fa4b46cae04fb972c6083ec01
mustafa <mryuksel5@gmail.com>
2023-03-22 00:04:18 +0300

    delete

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'ana_sayfa/ana_sayfa_view.dart';
+
 
 
 void main(List<String> args) async {
