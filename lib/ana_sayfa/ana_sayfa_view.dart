7b7c225190f7c3d13e8b1666bcb30da50055b271
mustafa <mryuksel5@gmail.com>
2023-03-21 23:57:21 +0300

    önceki kodlar silindi

--- lib/ana_sayfa/ana_sayfa_view.dart
+++ lib/ana_sayfa/ana_sayfa_view.dart
@@ -1,14 +1,15 @@
-import 'package:firebase_chat_app/register/register_bloc.dart';
-import 'package:firebase_chat_app/register/register_event.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../auth_islemleri/bloc/login_bloc.dart';
 
 class AnaSayfaView extends StatelessWidget {
   const AnaSayfaView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final bloc = context.read<RegisterBloc>();
+    final bloc = context.read<LoginBloc>();
     return Scaffold(
       appBar: AppBar(
         title: Text("deneme"),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:firebase_chat_app/register/register_bloc.dart';
-import 'package:firebase_chat_app/register/register_page.dart';
-import 'package:firebase_chat_app/register/register_view.dart';
+import 'package:firebase_chat_app/login/login_bloc.dart';
+import 'package:firebase_chat_app/login/login_page.dart';
+import 'package:firebase_chat_app/login/register_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,7 +13,7 @@ void main(List<String> args) async {
   await Firebase.initializeApp();
   runApp(
     BlocProvider(
-      create: (context) =>RegisterBloc(),
+      create: (context) =>LoginBloc(),
       child: const Myapp(),
     )
   );
@@ -31,7 +31,7 @@ class Myapp extends StatelessWidget {
             useMaterial3: true,
         ),
         /// eğer currentUser varsa kullanıcı daha önceden giriş yapmış demktir. Ana sayfayı yönlendiririz.
-        home: context.read<RegisterBloc>().auth.currentUser == null? const RegisterView() : const AnaSayfaView(),
+        home: context.read<LoginBloc>().auth.currentUser == null? const RegisterView() : const AnaSayfaView(),
     );
   }
 }
