17e7d224e0c57e04f3699f3995fa64913e8df3d2
mustafa <mryuksel5@gmail.com>
2023-03-22 20:13:18 +0300

    giriş modulü eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,4 @@
+import 'package:firebase_chat_app/app.dart';
+import 'package:flutter/widgets.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-
-
-
-void main(List<String> args) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(
-    const Myapp(),
-  );
-}
-
-class Myapp extends StatelessWidget {
-  const Myapp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-      
-    return MaterialApp(
-      
-        theme: ThemeData(
-            useMaterial3: true,
-        ),
-        
-        home: const ,
-    );
-  }
-}
-
+void main() => runApp(const App());
